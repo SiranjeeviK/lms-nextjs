666f57677c0bff4d8c5703962f62b2e168c13606
Siranjeevi K <81350268+SiranjeeviK@users.noreply.github.com>
2024-11-25 10:28:32 +0530

    feat: Add rich text editor and preview components, update chapter title form, and implement API for chapter updates

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,60 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      courseId: string;
+      chapterId: string;
+    };
+  }
+) {
+  try {
+    const { userId } = await auth();
+    const {isPublished, ...values} = await req.json();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+    });
+
+    if (!chapter) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    const updatedChapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    // TODO: handle video upload
+    
+
+    return NextResponse.json(updatedChapter);
+  } catch (error) {
+    console.error("[COURSE CHAPTER ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
